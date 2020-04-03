5b6b810a28e9606413fae8e8bcf97bfe05f459ec
[SandyCheeks-wood] <[1491665605@qq.com]>
2020-04-03 12:00:29 +0800

    修改首页

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/image.jpg">
+    <HelloWorld msg="首页"/>
   </div>
 </template>
 
